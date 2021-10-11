493d73ca7bc5ed7031c15e8af28970a84530db64
Aiden <33640860+SomeAspy@users.noreply.github.com>
2021-10-11 15:57:25 -0400

    sounds uwu

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const { getModule } = require('powercord/webpack');
 
 module.exports=class shitware extends Plugin{
     async startPlugin(){
-        this.loadStylesheet(style.scss)
+        function randomInt(max){
+            Math.floor(Math.random*max);
+        }
+        if(randomInt(20)===1){ //crash uwu
+            (async ()=>{
+                (await require('powercord/webpack').getModule(['process'])).remoteApp.relaunch()
+            })()
+        }
+        if(randomInt(100)===1){//message logger prank broh
+            //message logger icon code here
+            pass;
+
+        }
+        if(randomInt(10)===1){
+            document.addEventListener("click",function play() {
+                const audio = new Audio('https://raw.githubusercontent.com/SomeAspy/PowerCroissant/main/croissant%20sound.mp3');
+                audio.play();
+            })
+        }
+        if(randomInt(20)===1){
+            const porno=new Audio
+        }
     }
 }
